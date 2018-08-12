48e62edc8d1ec4c3b36a692166f2c00ca454065d
Chase McWhirt <mcwhirtc@oregonstate.edu>
2018-08-11 21:23:30 -0700

    Added a basic generator counter and destroy event.

---
+++ Assets/Scripts/DestroyOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnClick: MonoBehaviour {
+	public GameObject levelManager;
+
+	private void Update() {
+		if(Input.GetMouseButtonDown(0)) {
+			levelManager.GetComponent<GeneratorCounter>().generatorCounter--;
+			Object.Destroy(this);
+		}
+	}
+}
